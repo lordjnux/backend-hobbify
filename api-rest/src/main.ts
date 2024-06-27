166c054b47098bd5fb827f7595fffdbe63e411bb
Jeroham Sanchez <jeroham.sanchez@gmail.com>
2024-06-26 21:50:38 -0500

    swagger: config inicia :new

--- api-rest/src/main.ts
+++ api-rest/src/main.ts
@@ -4,12 +4,22 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { auth } from 'express-openid-connect';
 import { Auth0Config } from './config/auth0.config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 dotenv.config({ path: './.env.development.local' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(auth(Auth0Config));
+
+  const config = new DocumentBuilder()
+    .setTitle('HOBBIFY API')
+    .setDescription('The Hobbify API description')
+    .setVersion('1.0')
+    .addTag('hobbies')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT);
 }
