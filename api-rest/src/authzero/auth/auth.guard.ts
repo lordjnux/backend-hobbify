20c2a192c4538edd7d87b8830d51faeb921d30b2
Jeroham Sanchez <jeroham.sanchez@gmail.com>
2024-06-27 20:12:45 -0500

    authguard: only a comment added

--- api-rest/src/authzero/auth/auth.guard.ts
+++ api-rest/src/authzero/auth/auth.guard.ts
@@ -16,29 +16,29 @@ export class AuthGuard implements CanActivate {
   ): boolean | Promise<boolean> | Observable<boolean> {
     const httPContext = context.switchToHttp();
     const request = httPContext.getRequest();
-    // const pathRequested = request.url;
-    // const method = request.method;
-    // const body = request.body;
-    // const params = request.params;
-    // const query = request.query;
-    // const oidc = request.oidc;
-    // const oidcToken = request.oidc.accessToken;
+    const pathRequested = request.url;
+    const method = request.method;
+    const body = request.body;
+    const params = request.params;
+    const query = request.query;
+    const oidc = request.oidc;
+    const oidcToken = request.oidc.accessToken;
 
     //! PENDING: Sincronizar con equipo de frontend para la config de auth0 allá
-    //! variables de 
+    //! variables de identificación como mismo cliente de Auth0.
     const authHeader = request.headers.authorization;
-
     if (!authHeader) {
-      throw new UnauthorizedException('Authorization header is missing');
+      console.log('Authorization header is missing -- token perdido');
+      throw new UnauthorizedException('Authorization header is missing -- token perdido');
     }
 
-    // console.log('Aplico este guard....!');
-    // console.log('oidc:');
-    // console.log(JSON.stringify(oidc));
-    // console.log('oidc-user:');
-    // console.log(oidc.user);
-    // console.log('token:', oidcToken);
-    // console.log(method, pathRequested, params, query, body);
+    console.log('Aplico este guard....!');
+    console.log('oidc:');
+    console.log(JSON.stringify(oidc));
+    console.log('oidc-user:');
+    console.log(oidc.user);
+    console.log('token:', oidcToken);
+    console.log(method, pathRequested, params, query, body);
     // console.log(request);
 
     const token = authHeader.split(' ')[1];
